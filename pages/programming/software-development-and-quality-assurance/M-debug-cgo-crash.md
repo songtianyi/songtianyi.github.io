32abc53ede94747eccd4e2e8f62485c486b859eb
songtianyi <songtianyi@sky-cloud.net>
2022-06-07 05:22:21 +0800

    refact

--- pages/programming/software-development-and-quality-assurance/M-debug-cgo-crash.md
+++ pages/programming/software-development-and-quality-assurance/M-debug-cgo-crash.md
@@ -1,34 +1,34 @@
-# cgo crash debuging
+# cgo crash debugging
 
 Author: [songtianyi](http://songtianyi.info) create@2020-11-17
 
 ### ulimit setting
 
-``` 
+```
 
 ulimit -c unlimited
 ```
 
 ### generate core dump
 
-``` 
+```
 
 GOTRACEBACK=crash ./your-binary
 ```
 
 ### gdb
 
-``` 
+```
 
 gdb ./your-binary core.xxx
 ```
 
-``` 
+```
 
 bt full
 ```
 
-``` 
+```
 
 where
 ```
--- wc.sh
+++ wc.sh
@@ -32,25 +32,33 @@ echo "Career: $career"
 dc=$(wc_files 'pages/data-compression/*-comp.html')
 echo "Data compression: $dc"
 
+# x-sports
+xsports=$(wc_files 'pages/xsports/vietnam*.html pages/xsports/*.md')
+echo "X-sports: $xsports"
+
 # life
-life=$(wc_files 'pages/life/*-life.html pages/life/master-of-sex.html pages/life/*.md')
+life=$(wc_files 'pages/life/*-life.html pages/life/master-of-sex.html pages/life/talk-to-me.html pages/life/*.md')
 echo "Life: $life"
 
 # pieces
-pieces=$(wc_files 'pages/pieces/*-other.html pages/pieces/songtianyi.dump.html pages/pieces/*.md pages/pieces/blog-stats.html')
+pieces=$(wc_files 'pages/pieces/kindle-touch.html pages/pieces/native-english.html pages/pieces/how-to-write-your-blog.html pages/pieces/songtianyi.dump.html pages/pieces/*.md pages/pieces/blog-stats.html')
 echo "Pieces: $pieces"
 
 # programming
-programming=$(wc_files 'pages/programming/*/*.md')
+programming=$(wc_files 'pages/programming/*/*.md pages/programming/data-structure-and-algorithms/rsa.html pages/programming/software-frameworks/ionic-dev.html pages/programming/software-frameworks/spring-boot-and-websocket.html pages/programming/uncategorized/questions-and-answers.html pages/programming/software-development-and-quality-assurance/elk-practice.html pages/programming/programming-languages/about-shell.html pages/programming/programming-languages/html5-intro.html pages/programming/programming-languages/tencent-parser.html')
 echo "Programming: $programming"
 
-# secure
-secure=$(wc_files 'pages/secure/*-secure.html pages/secure/*.md')
-echo "Secure: $secure"
+# blockchain
+blockchain=$(wc_files 'pages/blockchain/*.md')
+echo "Blockchain: $blockchain"
 
 # VDI
 vdi=$(wc_files 'pages/vdi/*-vdi.html')
 echo "VDI: $vdi"
+
+# research
+research=$(wc_files 'pages/academic-research/*/*.md')
+echo "Research: $research"
 
 # mdk
 mdk=$(wc_files 'mdks/*.mdk')
@@ -58,5 +66,5 @@ mdk=$(wc_files 'mdks/*.mdk')
 # index
 index=$(wc_files 'index.html')
 
-echo "Total: " `expr $acm + $career + $life + $pieces + $programming + $secure + $vdi + $mdk + $index`
+echo "Total: " `expr $acm + $career + $dc + $xsports + $life + $pieces + $programming + $blockchain + $vdi + $research + $mdk + $index`
 
